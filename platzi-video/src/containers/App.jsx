fbae60623371d857d38bac7484724494fd390538
Miguel Prada <mapra99@gmail.com>
2020-09-20 13:14:44 -0500

    search component

--- platzi-video/src/containers/App.jsx
+++ platzi-video/src/containers/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../assets/styles/containers/App.scss';
 import Header from '../components/Header';
+import Search from '../components/Search';
 
 const App = () => (
   <div className='app'>
     <Header />
+    <Search />
   </div>
 );
 
