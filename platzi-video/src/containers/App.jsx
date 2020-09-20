b24fe304f140ed37701e7dc3ed0b2216515c1d81
Miguel Prada <mapra99@gmail.com>
2020-09-20 13:06:51 -0500

    header component

---
+++ platzi-video/src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/containers/App.scss';
+import Header from '../components/Header';
+
+const App = () => (
+  <div className='app'>
+    <Header />
+  </div>
+);
+
+export default App;
