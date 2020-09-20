b4a912fc09dc2000e0b4b9b163059833eee7b644
Miguel Prada <mapra99@gmail.com>
2020-09-20 13:27:46 -0500

    carousel components

--- platzi-video/src/containers/App.jsx
+++ platzi-video/src/containers/App.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import '../assets/styles/containers/App.scss';
 import Header from '../components/Header';
 import Search from '../components/Search';
+import Category from '../components/Category';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
 
 const App = () => (
   <div className='app'>
     <Header />
     <Search />
+    <Category>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Category>
   </div>
 );
 
