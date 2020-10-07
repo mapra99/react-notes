0c275df6ae7755b732d38f3bffb434c38df7bb9a
Miguel Prada <mapra99@gmail.com>
2020-10-07 03:28:38 -0500

    Navbar component

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 const container = document.getElementById('app');
-ReactDOM.render(<Badge 
+ReactDOM.render(<BadgeNew
                   firstName="Miguel" 
                   lastName="Prada" 
                   jobTitle="Frontend Engineer" 
