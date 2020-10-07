df7d32e1cf1f16ab570c57d87dd672c3dbe11730
Miguel Prada <mapra99@gmail.com>
2020-10-07 15:30:05 -0500

    component state

--- platzi-badges/src/components/BadgeForm.js
+++ platzi-badges/src/components/BadgeForm.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
-  handleChange(event) {
-
+  state = {
+    firstName: '',
+    lastName: '',
+    email: ''
   }
 
-  handleSubmit(event) {
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    console.log("adasdas")
   }
 
   render() {
+    const {firstName, lastName, email} = this.state;
     return(
       <div>
         <h1>New Attendant</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>First Name</label>
-            <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
+            <input onChange={this.handleChange} className="form-control" type="text" name="firstName" value={firstName} />
+          </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input onChange={this.handleChange} className="form-control" type="text" name="lastName" value={lastName} />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input onChange={this.handleChange} className="form-control" type="email" name="email"  value={email} />
           </div>
 
           <button className="btn btn-primary">
