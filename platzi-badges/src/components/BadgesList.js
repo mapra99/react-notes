43f0c50b01c72fde5b72b389ef5f403955bc8774
Miguel Prada <mapra99@gmail.com>
2020-10-11 15:31:47 -0500

    api get call

--- platzi-badges/src/components/BadgesList.js
+++ platzi-badges/src/components/BadgesList.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class BadgesList extends React.Component {
   render() {
     const {badges} = this.props;
+
+    if (badges.length === 0) {
+      return (
+        <div>
+          <h3>No encontramos ningún badge</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      )
+    }
 
     return (
       <ul className="list-unstyled">
