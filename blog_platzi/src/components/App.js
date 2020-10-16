a1959d82b127125f4673b553ab146371296dc2d7
Miguel Prada <miguelprada@MacBook-Pro-de-Miguel.local>
2020-10-15 22:03:04 -0500

    first install

---
+++ blog_platzi/src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const App = () => {
+  return (
+  <div className="margen">
+    <table className="tabla">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Miguel</td>
+          <td>mapra99@gmail.com</td>
+          <td>mapra99.com</td>
+        </tr>
+        <tr>
+          <td>Miguel</td>
+          <td>mapra99@gmail.com</td>
+          <td>mapra99.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  )
+}
+
+export default App;
