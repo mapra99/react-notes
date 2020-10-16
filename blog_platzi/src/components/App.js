a705adb77ef68554a4c453acaab9a4002f63998f
Miguel Prada <miguelprada@MacBook-Pro-de-Miguel.local>
2020-10-15 22:49:26 -0500

    fetching data on lifecycle method

--- blog_platzi/src/components/App.js
+++ blog_platzi/src/components/App.js
@@ -1,30 +1,27 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      usuarios: [
-        {
-          name: 'Miguel',
-          email: 'mapra99@gmail.com',
-          url: 'miguel.com'
-        },
-        {
-          name: 'Miguel',
-          email: 'mapra99@gmail.com',
-          url: 'miguel.com'
-        }
-      ]
+      usuarios: []
     }
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    this.setState({
+      usuarios: respuesta.data
+    })
   }
   
   ponerFilas = () => [
     this.state.usuarios.map(usuario => (
-      <tr>
+      <tr key={usuario.id}>
         <td>{usuario.name}</td>
         <td>{usuario.email}</td>
-        <td>{usuario.url}</td>
+        <td>{usuario.website}</td>
       </tr>
     ))
   ]
